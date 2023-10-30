92b864f3c54149723641411cfd8c56e1316d5ec9
Painbow <mylittleporygon@gmail.com>
2023-10-30 23:58:11 +0800

    commit again

--- CafeStaff/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class StaffSlotForm(FlaskForm):
-    shift_type = StringField('Shift Type', validators=[DataRequired()])
-    date = StringField('Date', validators=[DataRequired()])
-    status = StringField('Status', validators=[DataRequired()])
-    submit = SubmitField('Create Slot')
